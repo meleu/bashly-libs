d82a21efbf69a2e2c586722634cd03e41cdb1c2b
meleu <8508804+meleu@users.noreply.github.com>
2026-03-19 12:15:22 -0300

    Add bashly-ugrade marker
    Also change comments to be hidden commets, as explained here:
    https://bashly.dev/usage/writing-your-scripts/#hidden-comments

--- logs/logs.sh
+++ logs/logs.sh
@@ -1,42 +1,44 @@
-# Print a debug message to stderr (only when DEBUG is set and non-empty).
-#
-# Example:
-# $ DEBUG=1 log_debug "Debug information"
-# [2026-03-18 14:30:00] DEBUG: Debug information
+## [@bashly-upgrade]
+
+## Print a debug message to stderr (only when DEBUG is non-empty).
+##
+## Example:
+## $ DEBUG=1 log_debug "Debug information"
+## [2026-03-18 14:30:00] DEBUG: Debug information
 log_debug() {
   if [[ -n "${DEBUG:-}" ]]; then
     printf '[%s] DEBUG: %s\n' "$(logs_timestamp)" "$*" >&2
   fi
 }
 
-# Print an info message to stderr.
-#
-# Example:
-# $ log_info "Starting script"
-# [2026-03-18 14:30:00] INFO: Starting script
+## Print an info message to stderr.
+##
+## Example:
+## $ log_info "Starting script"
+## [2026-03-18 14:30:00] INFO: Starting script
 log_info() {
   printf '[%s] INFO: %s\n' "$(logs_timestamp)" "$*" >&2
 }
 
-# Print a warning message to stderr.
-#
-# Example:
-# $ log_warn "Warning message"
-# [2026-03-18 14:30:00] WARN: Warning message
+## Print a warning message to stderr.
+##
+## Example:
+## $ log_warn "Warning message"
+## [2026-03-18 14:30:00] WARN: Warning message
 log_warn() {
   printf '[%s] WARN: %s\n' "$(logs_timestamp)" "$*" >&2
 }
 
-# Print an error message to stderr.
-#
-# Example:
-# log_error "Error occurred"
-# [2026-03-18 14:30:00] ERROR: Error occurred
+## Print an error message to stderr.
+##
+## Example:
+## log_error "Error occurred"
+## [2026-03-18 14:30:00] ERROR: Error occurred
 log_error() {
   printf '[%s] ERROR: %s\n' "$(logs_timestamp)" "$*" >&2
 }
 
-# Return a formatted timestamp for log messages.
+## Return a formatted timestamp for log messages.
 logs_timestamp() {
   date +'%Y-%m-%d %H:%M:%S'
 }
